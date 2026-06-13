9a6b668caf517d2bf58c979eb9962a45c483ac36
MahirKumar <mahir.kumar@s.amity.edu>
2026-06-13 16:41:37 +0530

    Mahir

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 const PORT = process.env.PORT ?? 8080
 
 app.get('/', (req, res) => {
-    console.log('Hello World!');
+    return res.json({
+        message: 'Hello World! Mahir Kumar.'
+    })
     
 })
 
